3d8e4338021aee201eaf513e2c6d4cd9c9ae066b
anthogez <antonio.gomes@snyk.io>
2021-07-06 11:41:18 +0300

    feat: file content as Uint8Array to sign

--- lib/signatures.ts
+++ lib/signatures.ts
@@ -1,11 +1,10 @@
 import * as fs from 'fs';
-
 import { SignatureResult } from './types';
 import { fossidNative } from 'fossid-native';
 
 export async function getSignature(filePath: string): Promise<SignatureResult> {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, (err, data) => {
+    fs.readFile(filePath, (err, data: Uint8Array) => {
       if (err) reject(err);
       resolve(JSON.parse(fossidNative.getSignature(filePath, data)));
     });
