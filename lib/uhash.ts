bd0f6e09a26701a9d804f42df68e4660b3261df2
Rob Guinness <84016089+robguinness-snyk@users.noreply.github.com>
2021-08-31 10:36:21 +0300

    adds uhash module

---
+++ lib/uhash.ts
@@ -0,0 +1,41 @@
+// This module implements the uhash specification (v1.0) described in docs/uhash1-0
+import * as xxhash from 'xxhash';
+import * as crypto from 'crypto';
+import { SignatureResult } from './types';
+import { isBinary } from './utils/binary';
+
+const hashAlgorithm = 'md5';
+const regex = /\s/g;
+
+export const getUhashSignature = (
+  filePath: string,
+  fileContents: Buffer,
+): SignatureResult => {
+  const digest = getUhashDigest(fileContents, isBinary(fileContents));
+  const data = digest.slice(0, 24);
+  return {
+    path: filePath,
+    hashes_ffm: [
+      {
+        data,
+        format: 3,
+      },
+    ],
+  };
+};
+
+const getUhashDigest = (fileContents: Buffer, isBinary: boolean): string => {
+  const file = isBinary
+    ? fileContents
+    : removeWhiteSpace(fileContents.toString());
+  if (hashAlgorithm == 'md5') {
+    const hash = crypto.createHash(hashAlgorithm).update(file);
+    return hash.digest('hex');
+  } else {
+    return xxhash.hash64(fileContents, 0xcafebabe).digest();
+  }
+};
+
+const removeWhiteSpace = (fileContents: string): string => {
+  return fileContents.replace(regex, '');
+};
---
+++ test/uhash.test.ts
@@ -0,0 +1,43 @@
+import { promises } from 'fs';
+import * as path from 'path';
+import { SignatureResult } from '../lib/types';
+import { getUhashSignature } from '../lib/uhash';
+const { readFile } = promises;
+
+describe('getUhashSignature', () => {
+  it('returns correct SignatureResult for binary file', async () => {
+    const fixturePath = path.join(__dirname, 'fixtures');
+    const filePath = path.join(fixturePath, 'dog.jpg');
+    const fileContents = await readFile(filePath);
+    const expected: SignatureResult = {
+      path: filePath,
+      hashes_ffm: [
+        {
+          data: '95f0ec135ee38f07e07857d6',
+          format: 3,
+        },
+      ],
+    };
+    const actual = getUhashSignature(filePath, fileContents);
+    expect(actual.hashes_ffm[0].data.length).toEqual(24);
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('returns correct SignatureResult for text file', async () => {
+    const fixturePath = path.join(__dirname, 'fixtures');
+    const filePath = path.join(fixturePath, 'hello-world', 'add.cpp');
+    const fileContents = await readFile(filePath);
+    const expected: SignatureResult = {
+      path: filePath,
+      hashes_ffm: [
+        {
+          data: 'd53b2679a07eb6ab82e60dbb',
+          format: 3,
+        },
+      ],
+    };
+    const actual = getUhashSignature(filePath, fileContents);
+    expect(actual.hashes_ffm[0].data.length).toEqual(24);
+    expect(actual).toStrictEqual(expected);
+  });
+});
