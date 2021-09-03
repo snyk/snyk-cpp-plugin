c8bb9658dd14599a42f9ad2f9a395c275358e17e
Rob Guinness <84016089+robguinness-snyk@users.noreply.github.com>
2021-09-03 16:47:09 +0300

    cleanup

--- lib/uhash.ts
+++ lib/uhash.ts
@@ -1,5 +1,7 @@
-// This module implements the uhash specification (v1.0) described in docs/uhash1-0
+// This module implements the uhash specification (v1.0) described in docs/signature_specification.md
 // import * as xxhash from 'xxhash';
+// The version in this branch contains several different implementation options, which are intended for
+// benchmarking purposes
 import * as crypto from 'crypto';
 import { SignatureResult } from './types';
 import { isBinary } from './utils/binary';
@@ -27,19 +29,18 @@ export const getUhashSignature = (
 const getUhashDigest = (fileContents: Buffer, isBinary: boolean): string => {
   const file = isBinary
     ? fileContents
-    : // : removeWhiteSpace(fileContents.toString());
-      removeWhitespace(fileContents);
+    : removeWhitespace(fileContents);
   if (hashAlgorithm == 'md5') {
     const hash = crypto.createHash(hashAlgorithm).update(file);
     return hash.digest('hex');
   } else {
+    // placeholder for non-md5 hashing algorithm, e.g. xxhash
     // return xxhash.hash64(fileContents, 0xcafebabe).digest();
     return '';
   }
 };
 
 export const removeWhitespace = (fileContents: Buffer): Buffer => {
-  //return removeWhitespaceRegex2(fileContents);
   return removeWhitespaceBytewise(fileContents);
 };
 
@@ -50,6 +51,8 @@ export const removeWhitespaceRegex = (fileContents: Buffer): Buffer => {
 };
 
 // hex => integer as follows: 09 => 9, 0a => 10, 0b => 11, 0c => 12, 0d => 13, 20 => 32
+// WARNING: The below methods don't actually work! Was still searching for a
+// regex that could be used for operating on a hex string
 // const regex2 = /(09|0a|0b|0c|0d|20)(?=(?:[\da-f]{2})*$)/g;
 const regex2 = /(?:..)*?(09|0a|0b|0c|0d|20)/g;
 
@@ -87,18 +90,22 @@ export const removeWhitespaceBytewise5 = (fileBuffer: Uint8Array): Uint8Array =>
   return filteredBuffer;
 };
 
-const removeBytesFromBuffer5 = (
-  fileBuffer: Uint8Array,
+export const removeBytesFromBuffer = (
+  buffer: Buffer,
   valuesToRemove: Uint8Array,
-): Uint8Array => {
-  let writeIndex = 0;
-  for (let readIndex = 0; readIndex < fileBuffer.length; readIndex++) {
-    if (!valuesToRemove.includes(fileBuffer[readIndex])) {
-      fileBuffer[writeIndex] = fileBuffer[readIndex];
-      writeIndex++;
-    }
-  }
-  return fileBuffer.subarray(0, writeIndex);
+): Buffer => {
+  return buffer.filter((el) => {
+    return !valuesToRemove.includes(el);
+  }) as Buffer;
+};
+
+const removeBytesFromBuffer2 = (
+  buffer: Buffer,
+  valuesToRemove: number[],
+): Buffer => {
+  return buffer.filter((el) => {
+    return !valuesToRemove.includes(el);
+  }) as Buffer;
 };
 
 const removeBytesFromBuffer3 = (
@@ -131,20 +138,16 @@ const removeBytesFromBuffer4 = (
   return Buffer.from(filteredBuffer);
 };
 
-export const removeBytesFromBuffer = (
-  buffer: Buffer,
+const removeBytesFromBuffer5 = (
+  fileBuffer: Uint8Array,
   valuesToRemove: Uint8Array,
-): Buffer => {
-  return buffer.filter((el) => {
-    return !valuesToRemove.includes(el);
-  }) as Buffer;
-};
-
-const removeBytesFromBuffer2 = (
-  buffer: Buffer,
-  valuesToRemove: number[],
-): Buffer => {
-  return buffer.filter((el) => {
-    return !valuesToRemove.includes(el);
-  }) as Buffer;
+): Uint8Array => {
+  let writeIndex = 0;
+  for (let readIndex = 0; readIndex < fileBuffer.length; readIndex++) {
+    if (!valuesToRemove.includes(fileBuffer[readIndex])) {
+      fileBuffer[writeIndex] = fileBuffer[readIndex];
+      writeIndex++;
+    }
+  }
+  return fileBuffer.subarray(0, writeIndex);
 };
