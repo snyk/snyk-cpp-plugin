8c160cf348391748f26a5793fcc9c6956bfea937
Rob Guinness <84016089+robguinness-snyk@users.noreply.github.com>
2021-09-03 16:40:39 +0300

    adds support for both uhash and dubhash  (using a hard-coded flag

--- lib/signatures.ts
+++ lib/signatures.ts
@@ -1,8 +1,12 @@
 import { promises } from 'fs';
 import { SignatureResult } from './types';
 import { getUhashSignature } from './uhash';
+import { getDubHashSignature } from './dubhash';
 
 const { readFile } = promises;
+
+// This will be replaced with a proper argument after discussing with Antonio
+const SIGNATURE_FORMAT = 'dubhash';
 
 export async function getSignature(
   filePath: string,
@@ -10,8 +14,7 @@ export async function getSignature(
   try {
     const fileContents = await readFile(filePath);
     if (fileContents.length === 0) return null;
-
-    return getUhashSignature(filePath, fileContents);
+    return SIGNATURE_FORMAT === 'dubhash' ? getDubHashSignature(filePath, fileContents) : getUhashSignature(filePath, fileContents); 
   } catch (err) {
     console.error(err);
     throw err;
